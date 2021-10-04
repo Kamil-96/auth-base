29795164c4971efdbf09df469544f27028e1e270
Kamil Put <kamil.put@interia.pl>
2021-10-04 11:35:11 +0200

    Add new endpoints, modify 'logged' view

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  res.render('logged', { name: req.user.displayName, image: req.user.photos[0].value });
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', (req, res) => {
+  if(!req.user) res.redirect('/user/no-permission');
+  else res.render('profile');
+});
+
+router.get('/profile/settings', (req, res) => {
+  if(!req.user) res.redirect('/user/no-permission');
+  else res.render('profileSettings');
+});
 
 module.exports = router;
